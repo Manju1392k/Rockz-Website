0e34ff5635effe73ad016e06b769a724649bd842
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-06 13:47:45 +0530

    Rockz

--- RockzApp/forms.py
+++ RockzApp/forms.py
@@ -9,6 +9,6 @@ class RocketBlogForm(ModelForm):
         fields = ['Rocket_Name','Rocket_LaunchDate', 'Rocket_LandingDate', 'Rocket_Image']
 
     Rocket_Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputags'}))
-    Rocket_LaunchDate = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputags'}))
-    Rocket_LandingDate = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputags'}))
+    Rocket_LaunchDate = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputags', 'placeholder': 'DD-MM-YYYY'}))
+    Rocket_LandingDate = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputags', 'placeholder': 'DD-MM-YYYY'}))
     Rocket_Image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'imgtag'}))
