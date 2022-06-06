aca286492ff83b83b204af150fa8d7effe67fc75
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-06 12:37:56 +0530

    Rockz

--- RockzApp/models.py
+++ RockzApp/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class RocketBlog(models.Model):
+    Rocket_Name = models.CharField(max_length=150)
+    Rocket_LaunchDate = models.CharField(max_length=10)
+    Rocket_LandingDate = models.CharField(max_length=10)
+    Rocket_Image = models.ImageField(upload_to = '/photos')
+    
+    def __str__(self):
+        return self.Rocket_Name
