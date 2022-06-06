87e21139920c35d015e971472f089360874899ff
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-06 13:51:00 +0530

    Rockz

--- RockzApp/views.py
+++ RockzApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import RocketBlogForm
 
+from django.contrib import messages
+
 # Create your views here.
 def Home(request):
     form = RocketBlogForm
@@ -8,5 +10,6 @@ def Home(request):
         form = RocketBlogForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your Blog has been added successfully')
             return redirect('home')
     return render(request, 'index.html', {'form':form})
